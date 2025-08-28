160db111b748f82e80d7c2cb6860e9bb31180629
Dariiius <dariusweek.2018@gmail.com>
2025-08-29 01:12:45 +0800

    Fix error in views

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from ka1den.ka1den.settings import S3_URL
+from ka1den.settings import S3_URL
 
 def index(request):
     return render(request, 'home/index.html')
