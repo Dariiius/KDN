9a0879bd96498657331fb8b5f626222ff8f81417
Darius Lingo <86967617+Dariiius@users.noreply.github.com>
2025-09-01 22:23:59 +0800

    Update settings.py

--- ka1den/settings.py
+++ ka1den/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = '/home/ubuntu/KDN/ka1den/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
