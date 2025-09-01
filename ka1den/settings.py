ee458efe366ea7be6fab7f3f86d0aeaae7e97982
Darius Lingo <86967617+Dariiius@users.noreply.github.com>
2025-09-01 22:33:28 +0800

    Update settings.py

--- ka1den/settings.py
+++ ka1den/settings.py
@@ -119,9 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
