0a6d015a0def0c4e15679838f8b7198a54df6df8
Dariiius <dariusweek.2018@gmail.com>
2025-09-02 09:42:57 +0800

    Added whitenoise

--- ka1den/settings.py
+++ ka1den/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
+    'whitenoise.runserver_nostatic',
     
     # APPS
     'home',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
